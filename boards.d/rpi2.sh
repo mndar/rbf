ccce48e1a63ae6ab7e97278e701418fa9a2d226b
Mandar Joshi <emailmandar@gmail.com>
2015-06-23 23:21:12 +0530

    Extact files to vfat partition ignoring creation date and user

--- boards.d/rpi2.sh
+++ boards.d/rpi2.sh
@@ -9,6 +9,6 @@ ROOTUUID=$7
 
 #Enter Custom Commands Below
 echo "Extracting Boot Files"
-tar Jxvf $ROOTFILES -C $ROOTPATH
+tar Jxvomf $ROOTFILES -C $ROOTPATH
 sed -i 's/mmcblk0p3/mmcblk0p'$ROOTPARTINDEX'/' $ROOTPATH/boot/cmdline.txt
 exit 0
