30a2b843e23aa6c5ad98d625a7b4cedf725ef139
Mandar Joshi <emailmandar@gmail.com>
2015-07-18 20:34:18 +0530

    Set IPv6_rpfilter=no in firewalld.conf
    Required for CentOS

--- boards.d/rpi2.sh
+++ boards.d/rpi2.sh
@@ -11,4 +11,5 @@ ROOTUUID=$7
 echo "Extracting Boot Files"
 tar Jxvomf $ROOTFILES -C $ROOTPATH
 sed -i 's/mmcblk0p3/mmcblk0p'$ROOTPARTINDEX'/' $ROOTPATH/boot/cmdline.txt
+sed -i 's/IPv6_rpfilter=yes/IPv6_rpfilter=no/' $ROOTPATH/etc/firewalld/firewalld.conf
 exit 0
