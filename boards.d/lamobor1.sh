de4d75b1fd5f37c907560486619b893fc8d50ed8
Mandar Joshi <emailmandar@gmail.com>
2015-07-20 19:23:46 +0530

    Experimental Support of Lamobo R1

---
+++ boards.d/lamobor1.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+DISKIMAGE=$1
+STAGE1LOADER=$2
+UBOOT=$3
+ROOTPATH=$4
+ROOTFILES=$5
+ROOTPARTINDEX=$6
+ROOTUUID=$7
+
+#Enter Custom Commands Below
+echo "Writing U-Boot Image"
+dd if=$UBOOT of=$DISKIMAGE bs=1024 seek=8 status=noxfer
+
+exit 0
